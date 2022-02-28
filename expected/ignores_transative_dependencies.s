fe2f5541a306dd070f540bf1dc7a37cdd9bdc6cb
Adam Hess <adamhess1991@gmai.com>
2022-02-28 22:10:21 +0000

    Update ignore_transative example to make the transative-ness more clear

--- expected/ignores_transative_dependencies.s
+++ expected/ignores_transative_dependencies.s
@@ -11,8 +11,8 @@ c0 add $r0.6 = $r0.2, (0x01)
 ;;
 c0 add $r0.3 = $r0.1, (0x01)
 ;;
-c0 add $r0.4 = $r0.4, $r0.3
-c0 add $r0.7 = $r0.7, $r0.3
+c0 add $r0.4 = $r0.1, $r0.3
+c0 add $r0.7 = $r0.1, $r0.3
 c0 add $r0.8 = $r0.1, $r0.3
 ;;
 #### END BASIC BLOCK ####
